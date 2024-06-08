b8a4f2c581063d4f7d024ea8d1e407d15c8e0196
Jinhjlog <dndn2615@gmail.com>
2024-06-08 22:24:15 +0900

    second_commit

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import express, { Application } from 'express';
+import { userRouter } from './modules/users/route';
 
 const startServer = async () => {
   const app: Application = express();
 
   const port = process.env.PORT || 3000;
 
+  const router = express.Router();
+  router.use('/users', userRouter);
+
+  app.use('/api', router);
+
   app.listen(port, () => {
     console.log(`Server running on ${port}`);
-  });
-
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
   });
 };
 
